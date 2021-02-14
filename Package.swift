4ecf270ba99b205fa1f0e18fbc035ea0de8c3d9f
Ricardo <odrakir@gmail.com>
2021-02-14 09:22:57 +0100

    Fix package

--- Package.swift
+++ Package.swift
@@ -1,12 +1,10 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "GithubAPI",
+    platforms: [.macOS(.v10_15), .iOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "GithubAPI",
             targets: ["GithubAPI"]),
@@ -15,11 +13,9 @@ let package = Package(
         .package(url: "https://github.com/Odrakir/CombineNetworking", from: "0.1.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GithubAPI",
-            dependencies: []),
+            dependencies: ["CombineNetworking"]),
         .testTarget(
             name: "GithubAPITests",
             dependencies: ["GithubAPI"]),
